29821acf9fcdeee460344f33ca473b892f6b3536
Sanchi Chopra <35348387+SanchiChopra@users.noreply.github.com>
2020-11-15 19:09:15 +0530

    structure modification

---
+++ quizup/src/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import "./assets/style.css";
+
+class QuizUp extends Component {
+
+    render() {
+        return(
+            <div className = "container">
+                <div className = "title">
+                    QuizUp 
+                </div>
+            </div>
+        );
+    }
+}
