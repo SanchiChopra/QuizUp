92024dd6c6acb98dd7e456e4aa9b3e4ad632d2e2
Sanchi Chopra <35348387+SanchiChopra@users.noreply.github.com>
2020-11-17 19:27:57 +0530

    root component

--- quizup/src/index.js
+++ quizup/src/index.js
@@ -1,8 +1,13 @@
 import React, {Component} from "react";
+import ReactDOM from "react-dom";
 import "./assets/style.css";
+import quizService from "./quizService";
 
 class QuizUp extends Component {
 
+    state = {
+        questionBank: []    //this (local state) is where those 5 questions will be stores once they are pulled in from the API
+    }
     render() {
         return(
             <div className = "container">
@@ -13,3 +18,5 @@ class QuizUp extends Component {
         );
     }
 }
+
+ReactDOM.render(<QuizUp />, document.getElementById("root"));
