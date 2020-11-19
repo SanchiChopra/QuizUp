30cf45494c97d3ee2752d6b27d5928e4d42378b6
Sanchi Chopra <35348387+SanchiChopra@users.noreply.github.com>
2020-11-19 15:49:39 +0530

    questions added to question Bank

--- quizup/src/index.js
+++ quizup/src/index.js
@@ -7,6 +7,19 @@ class QuizUp extends Component {
 
     state = {
         questionBank: []    //this (local state) is where those 5 questions will be stores once they are pulled in from the API
+    };
+    getQuestions = () => {   //invokes quizService API and populates the questionBAnk state var with results
+        quizService().then( question  => {
+            this.setState({    //used to update the state variable, never write to a state var directly, update it this way by setState
+                questionBank: question
+            });
+        });
+    };
+
+    //we need a way to run this function once this component loads up, hence componentDidMount
+
+    componentDidMount() {    //lifecycle mthod
+        this.getQuestions();
     }
     render() {
         return(
@@ -14,6 +27,13 @@ class QuizUp extends Component {
                 <div className = "title">
                     QuizUp 
                 </div>
+                {/* rendering data inside curly braces, first check if questionBank has any data to render, it will 
+                    attempt to render only if there is any data.
+                    
+                    Map function destructures the question objects and it will return back a set fo H4 tags with a question text. */}
+                {this.state.questionBank.length >0 && this.state.questionBank.map(
+                    ({question,answers, correct, questionId}) => <h4> {question} </h4>
+                )}
             </div>
         );
     }
